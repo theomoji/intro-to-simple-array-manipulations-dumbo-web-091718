614e6cc3b8edd7dab14b7446d9a9fa7e741db76c
Shuo Wang <gken90@gmail.com>
2018-09-02 17:53:46 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,8 +6,8 @@ def using_unshift(array, string)
   array.unshift(string)
 end
 
-def using_pop(array, string)
-  deleted_string = array.pop(string)
+def using_pop(array)
+  deleted_string = array.pop
   return deleted_string
 end
 
